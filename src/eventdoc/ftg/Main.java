2c25b86d27dbadb89a19b0a634df5be3ce8e3fb4
mmyers <mmyers@users.noreply.github.com>
2023-07-20 10:02:27 -0500

    Fix crash

--- src/eventdoc/ftg/Main.java
+++ src/eventdoc/ftg/Main.java
@@ -20,9 +20,9 @@ public class Main {
     public static final String NAME = "MichaelM's event documenter";
     public static final String VERSION = "1.06 FTG";
 
-    private static List<String> eventFiles; // include files
-    private static List<String> extraEventFiles; // files of events that aren't in any include file
     static String moddir;
+    private static List<String> eventFiles = new ArrayList<>(); // include files
+    private static List<String> extraEventFiles = new ArrayList<>(); // files of events that aren't in any include file
     private static String language;
     private static String baseDir;
     private static String outputDir;
@@ -60,7 +60,7 @@ public class Main {
             System.out.println("Mod directory is " + resolver.getModDirName());
         }
 
-        if (eventFiles == null || eventFiles.isEmpty()) {
+        if (eventFiles.isEmpty()) {
             System.err.println("No event file specified.");
             printUsage();
             System.exit(0);
